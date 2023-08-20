fca8de81c393b71b9a6e155819c1a2f1555cacd1
emiliouranides36 <142753983+emiliouranides36@users.noreply.github.com>
2023-08-20 15:34:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./app.sh && ./app.sh`,{
 	cwd: './static',
 	env: {
-		id: `${process.env.id||'3f094e96-0a64-40c9-9770-3e0147c18a34'}`,
+		id: `${process.env.id||'af7dc403-d62e-4aae-a148-46f6726b8c09'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
